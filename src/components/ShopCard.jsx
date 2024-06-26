0082d56050e1698903e9dd9279f2510dfe265eaf
Elizaveta Smekhova <smekhovaelizaveta@gmail.com>
2024-06-26 12:01:48 +0300

    task 2

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -1,10 +1,10 @@
 
 export const ShopCard = (card) => {
-    const {name, price, color, img} = card;
+    const {name, price, color, img} = card.card;
 
   return (
     <div className="card">
-        <h2 className="card-title">{name}</h2>
+       <h2 className="card-title">{name}</h2>
         <h5 className="card-color">{color}</h5>
             <div className="card-img">
                  <img src={img} alt={name} />
@@ -12,7 +12,9 @@ export const ShopCard = (card) => {
             <div className="card-add">
                 <span className="card-prise">{price}$</span>
                 <button className="card-btn">ADD TO CART</button>
+            
             </div>
+        
      </div>
   )
 }
--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,12 +1,12 @@
 export const ShopItem = (card) => {
-    const {name, price, color, img} = card;
+    const {name, price, color, img} = card.card;
   return (
-    <div>
-        <div className="list-img">
+    <div className="list">
+        <div className="card-img">
              <img src={img} alt={name} />
          </div>
-            <h2 className="list-title">{name}</h2>
-            <h5 className="list-color">{color}</h5>
+            <h2 className="card-title">{name}</h2>
+            <h5 className="card-color">{color}</h5>
             <span className="card-prise">{price}$</span>
             <button className="card-btn">ADD TO CART</button>
         </div>
